6822b2b3dfd95812c1baab939012d41636536cc3
Dadi Nur Ardhi <artdee.dna@gmail.com>
2022-02-03 02:05:07 +0800

    fix: clear warning

--- src/layouts/LandingPage.jsx
+++ src/layouts/LandingPage.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
 const LandingPage = (props) => {
   return <div className='LandingPage overflow-visible'>{props.children}</div>;
 };
