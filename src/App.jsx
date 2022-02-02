06d3154556f7021f8c586149d23faa7a4fe8d39d
Artieady Budiman <artieadyb@gmail.com>
2022-02-03 00:08:34 +0700

    feat: add productHightlight

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import ProductHighlight from './components/ProductHighlight'
+
 function App() {
   return (
     <div>
-      <h1>Sweethome</h1>
+      <ProductHighlight/>
     </div>
   );
 }
---
+++ src/components/ProductHighlight.jsx
@@ -0,0 +1,17 @@
+import kitchen from './../assets/kitchen.jpg';
+
+const ProductHighlight = () =>{
+   return(
+      <div className="productHighlight d-flex w-100 bg-primary">
+         <div className="productImage" >
+            <div className="ideasBar">
+
+            </div>
+         </div>
+      </div>
+   )
+};
+
+export default ProductHighlight;
+
+// style={{backgroundImage: `url(${kitchen})`}}
