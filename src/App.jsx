0b4e38861545765460f65b36287db02975c4ea91
Dadi Nur Ardhi <artdee.dna@gmail.com>
2022-02-03 01:02:45 +0800

    feat: finish layouting What We Do Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { HomePage } from './pages';
+
 function App() {
   return (
     <div>
       <h1>Sweethome</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as PenIcons } from './icons/svg/PenIcons';
+export { default as PaperPlane } from './icons/svg/PaperPlane';
+export { default as CupOfCoffee } from './icons/svg/CupOfCoffee';
