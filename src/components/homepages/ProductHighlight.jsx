1381dc08884d420a63959a669039b085308421f2
Artieady Budiman <artieadyb@gmail.com>
2022-02-04 17:35:41 +0700

    feat: add category function

--- src/components/homepages/ProductHighlight.jsx
+++ src/components/homepages/ProductHighlight.jsx
@@ -1,23 +1,43 @@
 import kitchen from '../../assets/kitchen.jpg';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const ProductHighlight = () =>{
+   const [category, setCategory] = useState('')
+   const [image, setImage] = useState({kitchen})
+
+   useEffect (()=>{
+      axios
+      .get(`https://team-b-see-event.herokuapp.com/api/v1/event?category=${category}`)
+      .then((response) => {
+         const data = response.data.result[0].image;
+         setImage(data)
+      })
+
+   }, [category])
+
    return(
-      <div className="productHighlight d-flex">
-         <div className="contentImage card">
-            <img src={kitchen} className="card-img" alt="..."/>
+      <div className="productHighlight d-flex ">
+         <div className="contentImage card mx-auto">
+            <img src={image} className="card-img imageHighlight" alt="..."/>
             <div className="contentOverlay d-flex align-items-end card-img-overlay">
               <div className="ideasBar d-flex bg-primary">
                  <div className="categoryFilter d-flex align-items-center justify-content-between m-auto text-light">
-                     <div>Get currated renovation idea for your</div>
+                     <div className="ideaCaption">Get currated renovation idea for your</div>
                      <div className="filterSelect">
-                        <select className="form-select text-light" id="inlineFormSelectPref">
-                           <option value="1">Kitchen</option>
-                           <option value="2">Living Room</option>
-                           <option value="3">Bed Room</option>
+                        <select 
+                        className="form-select text-light" 
+                        id="inlineFormSelectPref"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}>
+                           <option value=" ">All Category</option>
+                           <option value="Photography">Kitchen</option>
+                           <option value="Lifestyle">Living Room</option>
+                           <option value="Design">Bed Room</option>
                         </select>
                      </div>
                      <div className="seeAllButton">
-                        <button type="submit" className="btn btn-secondary">See All</button>
+                        <button type="submit" className="btn px-4 btn-secondary"><b>See All</b></button>
                      </div>
                  </div>
               </div>
