708befd41a2a913afd5be7fc13b0a890b51d2c3f
Dadi Nur Ardhi <artdee.dna@gmail.com>
2022-02-04 23:27:01 +0800

    fix: fix layouts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,14 +4,17 @@ import {
   OurWorkSection,
   WhatWeDoSection,
 } from '../components';
+import LandingPage from '../layouts/LandingPage';
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
-      <WhatWeDoSection />
-      <OurWorkSection />
-      <ProductHighlight />
-    </div>
+    <LandingPage>
+      <div className='Homepage'>
+        <WhatWeDoSection />
+        <OurWorkSection />
+        <ProductHighlight />
+      </div>
+    </LandingPage>
   );
 };
 
