604fb32556b6569211d283c4d8b90d93b9dbc79a
Artieady Budiman <artieadyb@gmail.com>
2022-02-10 03:01:07 +0700

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-// import { Route, Routes } from 'react-router-dom';
-// import { HomePage, Showcase, DashboardPage, NotFoundPages } from './pages';
-import { Project } from './pages';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage, Showcase, DashboardPage, NotFoundPages } from './pages';
 
 function App() {
   return (
-   //  <Routes>
-   //    <Route index element={<HomePage />} />
-   //    <Route path='showcase' element={<Showcase />} />
-   //    <Route path='dashboard' element={<DashboardPage />} />
-   //    <Route path='*' element={<NotFoundPages />} />
-   //  </Routes>
-   <Project/>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='showcase' element={<Showcase />} />
+      <Route path='dashboard' element={<DashboardPage />} />
+      <Route path='*' element={<NotFoundPages />} />
+    </Routes>
   );
 }
 
