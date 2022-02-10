aa09a0027d11dd93973633544cda13e7cf314ec4
mukti342 <aliamae342@gmail.com>
2022-02-10 10:33:15 +0700

    feature show case all vew

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import { HomePage, Showcase, DashboardPage, NotFoundPages } from './pages';
+// import { Route, Routes } from 'react-router-dom';
+// import { HomePage, Showcase, DashboardPage, NotFoundPages } from './pages';
+import ShowCaseAllVew from './components/homepages/ShowCaseAllVew';
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path='showcase' element={<Showcase />} />
-      <Route path='dashboard' element={<DashboardPage />} />
-      <Route path='*' element={<NotFoundPages />} />
-    </Routes>
+    // <Routes>
+    //   <Route index element={<HomePage />} />
+    //   <Route path='showcase' element={<Showcase />} />
+    //   <Route path='dashboard' element={<DashboardPage />} />
+    //   <Route path='*' element={<NotFoundPages />} />
+    // </Routes>
+    <ShowCaseAllVew/>
+    
   );
 }
 
