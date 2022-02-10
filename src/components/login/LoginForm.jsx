41a809d0c5c0619c5a986539211862ee99874585
Hairdo Prakoso <frog.edo@gmail.com>
2022-02-10 20:17:30 +0700

    Modal Login Fixed

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,50 +1,99 @@
-import React from 'react';
-import {Button, Form} from "react-bootstrap";
-import {BsGoogle} from "react-icons/bs";
-import {FaFacebookF} from "react-icons/fa";
-import {AiOutlineEyeInvisible} from "react-icons/ai";
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { BsGoogle } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
 
-const LoginForm = ({onClose}) => {
-    return (
+const LoginForm = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Please enter your email"),
+      password: Yup.string()
+        .min(8, "minimun 8 characters")
+        .max(10, "maximum 10 characters")
+        .required("Please enter your password"),
+    }),
+
+    onSubmit: (values) => {
+      const data = {
+        email: values.email,
+        password: values.password,
+      };
+    },
+  });
+  return (
+    <Modal
+      show={props.show}
+      fullscreen
+      size="lg"
+      aria-labelledby="contained-modal-login"
+      centered
+    >
+      <Modal.Body className="p-0">
         <div className="loginForm-wrapper">
-            <div className="background-login">
-                <div className="welcome">
-                    <h1>Welcome Back!</h1>
-                    <p>Rhoncus sed at nulla odio. Faucibus quam magna feugiat vitae in. 
-                    Risus et fermentum in risus nibh praesent netus bibendum</p>
-                </div>
+          <div className="background-login">
+            <div className="welcome-text">
+              <h1 className="textWelcome">Welcome Back!</h1>
+              <br />
+              <p>
+                Rhoncus sed at nulla odio. Faucibus quam magna feugiat vitae in.
+                <br />
+                Risus et fermentum in risus nibh praesent netus bibendum
+              </p>
             </div>
-            <div className="login">
-                <button onClick={onClose}className="login-close">&#10005;</button>
-                <div className="login-text">
-                    <h1 className="Text">Login</h1>
-                    <p>Don’t have account? <a href='#'>Sign Up</a></p>
-                </div>
-                <div className="form-login">
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                    </Form.Group>
-
+          </div>
+          <div className="login">
+            <button onClick={() => props.onHide()} className="login-close">
+              &#10005;
+            </button>
+            <div className="login-text">
+              <h1 className="Text">Login</h1>
+              <p>
+                Don’t have account? <a href="#">Sign Up</a>
+              </p>
+            </div>
+            <div className="form-login">
+              <Form onSubmit="{formik.handleSubmit}">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"/><AiOutlineEyeInvisible />
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                  <AiOutlineEyeInvisible />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                </div>
-                <Button className="button-login" variant="secondary"><b>Login</b></Button>
-                <div className="button-social">
-                <Button variant="mint"><BsGoogle /> &nbsp;Google</Button>
-                <Button variant="mint"><FaFacebookF/> &nbsp;Facebook</Button>
-                </div>
+              </Form>
             </div>
-            
+            <Button className="button-login" variant="secondary">
+              <b>Login</b>
+            </Button>
+            <div className="button-social">
+              <Button variant="mint">
+                <BsGoogle /> &nbsp;Google
+              </Button>
+              <Button variant="mint">
+                <FaFacebookF /> &nbsp;Facebook
+              </Button>
+            </div>
+          </div>
         </div>
-        
-    );
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default LoginForm;
--- src/components/nav/NavbarSection.jsx
+++ src/components/nav/NavbarSection.jsx
@@ -1,60 +1,65 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link, Route, Routes } from 'react-router-dom';
-import Logo from '../../assets/icons/svg/SweetHome.svg';
-import {useState} from 'react';
-import LoginForm from '../login/LoginForm';
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link, Route, Routes } from "react-router-dom";
+import Logo from "../../assets/icons/svg/SweetHome.svg";
+import { useState } from "react";
+import LoginForm from "../login/LoginForm";
 
 const NavbarSection = (props) => {
   const background = props.background;
 
-  const [showLogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
-    <div className='NavbarSection'>
+    <div className="NavbarSection">
       <Navbar
         collapseOnSelect
-        expand='md'
-        bg={background && 'white'}
-        fixed='top'
-        variant='light'>
+        expand="md"
+        bg={background && "white"}
+        fixed="top"
+        variant="light"
+      >
         <Container fluid>
-          <Navbar.Brand as={Link} to='/' className='p-2'>
-            <img src={Logo} alt='logo SweetHome' />
+          <Navbar.Brand as={Link} to="/" className="p-2">
+            <img src={Logo} alt="logo SweetHome" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Nav.Link as={Link} to='/services' className='fw-bold'>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/services" className="fw-bold">
                 Our Service
               </Nav.Link>
-              <Nav.Link as={Link} to='/showcase' className='fw-bold'>
+              <Nav.Link as={Link} to="/showcase" className="fw-bold">
                 Showcase
               </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link onClick={(e) => {
-                e.preventDefault()
-                setShowLogin(true)
-              }}
+              <Nav.Link
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowLogin(true);
+                }}
                 as={Link}
-                to='/login'
-                className='fw-bold text-secondary'>
+                to="/login"
+                className="fw-bold text-secondary"
+              >
                 Login
               </Nav.Link>
               <Nav.Link
                 as={Link}
-                to='/signup'
-                className='fw-bold text-secondary'>
+                to="/signup"
+                className="fw-bold text-secondary"
+              >
                 Sign Up
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
+        <LoginForm show={showLogin} onHide={() => setShowLogin(false)} />
       </Navbar>
 
-      {showLogin && <LoginForm onClose={() => {
+      {/* {showLogin && <LoginForm onClose={() => {
         setShowLogin(false)
-      }} />}
+      }} />} */}
       {/* <Routes>
         <Route path="/login" element={<LoginForm />} />
       </Routes>
